759a80bad111264efe6db4b2314252f08054dea2
caijf <369756941@qq.com>
2022-05-06 16:16:00 +0800

    feat: 新增统一社会信用代码查询器

---
+++ src/Query/SocialCreditCode/index.md
@@ -0,0 +1,13 @@
+---
+title: 统一社会信用代码
+group:
+  title: 数据查询
+---
+
+# 统一社会信用代码查询
+
+<Alert type="warning">
+  按 《<a href="https://zh.wikisource.org/zh-hans/GB_32100-2015_法人和其他组织统一社会信用代码编码规则" target="_blank">GB 32100-2015 法人和其他组织统一社会信用代码编码规则</a>》解析，仅供测试使用，请勿用于非法用途。
+</Alert>
+
+<code src="./index.tsx" inline />
---
+++ src/Query/SocialCreditCode/index.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import { Card, Button } from 'antd';
+import { BizForm, BizFormItemInput, BizDescriptions } from 'antd-more';
+import { isSocialCreditCode } from 'util-helpers';
+import { parseAreaCode } from 'lcn';
+import { organization } from '../../SocialCreditCode/constants';
+
+function QueryBankCard() {
+  const [data, setData] = React.useState<{
+    unifiedIdentifier: string;
+    organizationName: string;
+    organizationClass: string;
+    province: string;
+    city: string;
+    area: string;
+    bodyIdentifier: string;
+  } | null>(null);
+
+  return (
+    <Card>
+      <BizForm
+        submitter={false}
+        size="large"
+        pressEnterSubmit={false}
+        onFinish={({ unifiedIdentifier }) => {
+          const organizationCode = unifiedIdentifier.substring(0, 2);
+          const areaCode = unifiedIdentifier.substring(2, 8);
+          const bodyIdentifier = unifiedIdentifier.substring(8, 17);
+
+          const [province, city, area] = parseAreaCode(areaCode);
+          const currentOrganization = organization.find(
+            (item) => item.value === organizationCode[0],
+          );
+          const currentOrganizationClass = currentOrganization?.children.find(
+            (item) => item.value === organizationCode[1],
+          );
+
+          setData({
+            unifiedIdentifier,
+            bodyIdentifier,
+            organizationName: currentOrganization?.label || '',
+            organizationClass: currentOrganizationClass?.label || '',
+            province: province?.name || '',
+            city: city?.name || '',
+            area: area?.name || '',
+          });
+        }}
+        onFinishFailed={() => {
+          setData(null);
+        }}
+      >
+        <BizFormItemInput
+          name="unifiedIdentifier"
+          label="统一社会信用代码"
+          hideLabel
+          disabledWhiteSpace
+          inputProps={{
+            placeholder: '请输入统一社会信用代码',
+            maxLength: 18,
+          }}
+          required
+          extendRules={[
+            {
+              validator(rule, value) {
+                if (!isSocialCreditCode(value)) {
+                  return Promise.reject('请输入正确的统一社会信用代码');
+                }
+                return Promise.resolve();
+              },
+            },
+          ]}
+          contentAfter={
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+          }
+        />
+      </BizForm>
+      {data && (
+        <BizDescriptions column={1} bordered>
+          <BizDescriptions.Item label="统一社会信用代码">
+            {data.unifiedIdentifier}
+          </BizDescriptions.Item>
+          <BizDescriptions.Item label="登记管理部门">{data.organizationName}</BizDescriptions.Item>
+          <BizDescriptions.Item label="机构类别">{data.organizationClass}</BizDescriptions.Item>
+          <BizDescriptions.Item label="经营地址">{`${data.province}/${data.city}${
+            data.area ? '/' + data.area : ''
+          }`}</BizDescriptions.Item>
+          <BizDescriptions.Item label="主体标识码">{data.bodyIdentifier}</BizDescriptions.Item>
+        </BizDescriptions>
+      )}
+    </Card>
+  );
+}
+
+export default QueryBankCard;

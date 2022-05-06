13aa6c79eca53ace9511848bd6581c7440be5689
caijf <369756941@qq.com>
2022-05-06 15:32:49 +0800

    feat: 新增银行卡号查询器

---
+++ src/Query/BankCard/index.md
@@ -0,0 +1,11 @@
+---
+title: 银行卡号
+group:
+  title: 数据查询
+---
+
+# 银行卡号查询
+
+<Alert type="warning">仅供测试使用，请勿用于非法用途。</Alert>
+
+<code src="./index.tsx" inline />
---
+++ src/Query/BankCard/index.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { Card, Button, message } from 'antd';
+import { BizForm, BizFormItemInput, BizDescriptions } from 'antd-more';
+import { searchCardBin, CardInfo } from 'bankcard';
+
+function QueryBankCard() {
+  const [data, setData] = React.useState<
+    | (CardInfo & {
+        bankCardNo: string;
+      })
+    | null
+  >(null);
+
+  return (
+    <Card>
+      <BizForm
+        submitter={false}
+        size="large"
+        pressEnterSubmit={false}
+        onFinish={({ bankCard }) => {
+          const info = searchCardBin(bankCard);
+          if (info) {
+            setData({
+              ...info,
+              bankCardNo: bankCard,
+            });
+          } else {
+            message.error('找不到该银行卡号信息');
+          }
+        }}
+        onFinishFailed={() => {
+          setData(null);
+        }}
+      >
+        <BizFormItemInput
+          name="bankCard"
+          label="银行卡号"
+          hideLabel
+          disabledWhiteSpace
+          inputProps={{
+            placeholder: '请输入银行卡号 或 卡Bin',
+          }}
+          required
+          contentAfter={
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+          }
+        />
+      </BizForm>
+      {data && (
+        <BizDescriptions column={1} bordered>
+          <BizDescriptions.Item label="银行卡号 或 卡Bin">{data.bankCardNo}</BizDescriptions.Item>
+          <BizDescriptions.Item label="银行名称">{data.bankName}</BizDescriptions.Item>
+          <BizDescriptions.Item label="银行编码">{data.bankCode}</BizDescriptions.Item>
+          <BizDescriptions.Item label="卡类型">{data.cardTypeName}</BizDescriptions.Item>
+          <BizDescriptions.Item label="卡Bin">{data.cardBin}</BizDescriptions.Item>
+          <BizDescriptions.Item label="卡号长度">{data.len}</BizDescriptions.Item>
+        </BizDescriptions>
+      )}
+    </Card>
+  );
+}
+
+export default QueryBankCard;
